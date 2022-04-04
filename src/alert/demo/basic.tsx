46112ca8644b3df255840744a4a75a323f5d0907
hezhihang <917944345@qq.com>
2022-04-04 19:57:47 +0800

    test

---
+++ src/alert/demo/basic.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Alert } from "C:/Users/Ice/Desktop/testAntd";
+import "C:/Users/Ice/Desktop/testAntd/esm/alert/style";
+
+export default () => <Alert kind="warning">这是一条警告提示</Alert>;
